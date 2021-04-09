2202ff2816063520002686755e29bf3b7ab7d9aa
Goshika Amaresh <goshikaamaresh@gmail.com>
2021-04-09 09:42:00 -0700

    initial

---
+++ include/sidebar.php
@@ -0,0 +1,52 @@
+<div class="sidemenu-container navbar-collapse collapse fixed-menu">
+	                <div id="remove-scroll">
+	                    <ul class="sidemenu page-header-fixed p-t-20" data-keep-expanded="false" data-auto-scroll="true" data-slide-speed="200">
+	                        <li class="sidebar-toggler-wrapper hide">
+	                            <div class="sidebar-toggler">
+	                                <span></span>
+	                            </div>
+	                        </li>
+	                        <li class="sidebar-user-panel">
+	                            <div class="user-panel">
+	                                
+                                        <div class="profile-usertitle">
+                                            <div class="sidebar-userpic-name">Company Name </div>
+                                            <div class="profile-usertitle-job"> Subtitle </div>
+                                        </div>
+                                      
+	                            </div>
+	                        </li>
+							<li class="nav-item start">
+	                            <a href="item.php" class="nav-link nav-toggle">
+	                                <i class="material-icons">dashboard</i>
+	                                <span class="title">Home</span>
+	                            </a>
+	                         
+	                        </li>
+							<li class="nav-item start">
+	                            <a href="cart.php" class="nav-link nav-toggle">
+	                                <i class="material-icons">dashboard</i>
+	                                <span class="title">Cart</span>
+	                            </a>
+	                         
+	                        </li>
+	                         <li class="menu-heading">
+			                	<span>Menu1</span>
+			                </li>
+	                         <li class="menu-heading">
+			                	<span>Menu2</span>
+			                </li>
+	                         <li class="menu-heading">
+			                	<span>Menu3</span>
+			                </li>
+	                         <li class="menu-heading">
+			                	<span>Menu4</span>
+			                </li>
+	                         <li class="menu-heading">
+			                	<span>Menu5</span>
+			                </li>
+	                      
+	                    </ul>
+	                </div>
+                </div>
+            </div>
